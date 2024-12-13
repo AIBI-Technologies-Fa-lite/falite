32587cc2301393bdd04520e1717e676ed043b922
SharK-020 <dev.kunalshar0@gmail.com>
2024-12-13 07:38:03 +0000

    add client controller created

---
+++ server/src/controllers/admin/client.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import prisma from "../../db";
+import { apiResponse, CustomRequest } from "../../utils/response";
+
+export const createClient = async (req: Request, res: Response) => {
+  const user = (req as CustomRequest).user;
+  const { code, name } = req.body as { code: string; name: string };
+  try {
+    await prisma.client.create({
+      data: {
+        code,
+        name,
+        organization: {
+          connect: {
+            id: user.organizationId
+          }
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    apiResponse.error(res);
+  }
+};
