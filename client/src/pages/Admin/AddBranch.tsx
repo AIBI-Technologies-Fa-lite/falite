b414c0ea4bd9138755ab80418e3a08b5595c76a6
SharK-020 <dev.kunalshar0@gmail.com>
2025-01-13 05:53:35 +0000

    minor changes

--- client/src/pages/Admin/AddBranch.tsx
+++ client/src/pages/Admin/AddBranch.tsx
@@ -22,31 +22,37 @@ const AddBranch = () => {
     }
   };
   return (
-    <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-3 placeholder:text-gray-400">
-        <div className="flex flex-col col-span-1 gap-2">
-          <label>Branch Name {errors.name ? <span className="text-red-500">*</span> : null}</label>
+    <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
+      <div className='grid grid-cols-1 gap-6 md:grid-cols-3 placeholder:text-gray-400'>
+        <div className='flex flex-col col-span-1 gap-2'>
+          <label>
+            Branch Name{" "}
+            {errors.name ? <span className='text-red-500'>*</span> : null}
+          </label>
           <input
-            type="text"
+            type='text'
             {...register("name", { required: true })}
             className={`p-2 border-gray-500 rounded-lg border-2`}
-            placeholder="Branch Name"
+            placeholder='Branch Name'
           />
         </div>
-        <div className="flex flex-col col-span-1 gap-2">
-          <label>Branch Code {errors.code ? <span className="text-red-500">*</span> : null}</label>
+        <div className='flex flex-col col-span-1 gap-2'>
+          <label>
+            Branch Code{" "}
+            {errors.code ? <span className='text-red-500'>*</span> : null}
+          </label>
           <input
-            type="text"
+            type='text'
             {...register("code", { required: true })}
             className={`p-2 border-gray-500 rounded-lg border-2`}
-            placeholder="Branch Code"
+            placeholder='Branch Code'
           />
         </div>
       </div>
 
       <button
-        type="submit"
-        className="px-4 py-2 mt-6 text-white transition-all duration-100 bg-purple-600 rounded-lg hover:bg-purple-400 disabled:bg-purple-300"
+        type='submit'
+        className='px-4 py-2 mt-6 text-white transition-all duration-100 bg-purple-600 rounded-lg hover:bg-purple-400 disabled:bg-purple-300'
         disabled={isLoading}
       >
         Add Branch
